47b673618c3adee2d475a234ccd1206d9d496920
akibandsajid-art <akibandsajid@gmail.com>
2026-09-04 22:22:25 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "guess-the-person-v1";
-const ASSETS = ["/", "/index.html", "/style.css", "/script.js", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
+const ASSETS = ["/", "/index.html", "/style.css", "/script.js", "/manifest.webmanifest","/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
